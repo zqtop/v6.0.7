3564fb39f67f178d9b10f4dec29d0a062b63b229
Administrator <admin@example.com>
2021-08-06 23:16:18 +0800

    v6.0.7框架

---
+++ app/index/controller/Test.php
@@ -0,0 +1,18 @@
+<?php
+namespace  app\index\controller;
+use app\BaseController;
+/**
+ * Created by PhpStorm.
+ * User: php
+ * Date: 2021/8/6
+ * Time: 23:07
+ */
+class  Test extends  BaseController
+{
+
+
+    public function  index()
+    {
+        echo '2342432';
+    }
+}
